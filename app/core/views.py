a3d5eca65ceee702fff86af5eb4a9e62dd184973
Vasyl808 <v0961491914@gmail.com>
2024-09-17 13:07:56 +0300

    Fix error.

--- app/core/views.py
+++ app/core/views.py
@@ -1,11 +1,14 @@
 """
 Core views for app.
 """
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-@api_view(['GET'])
-def health_check(request):
+class HealthCheckView(APIView):
     """Returns successful response."""
-    return Response({'healthy': True})
+
+    def get(self, request):
+        return Response(
+            {'healthy': True}
+        )
